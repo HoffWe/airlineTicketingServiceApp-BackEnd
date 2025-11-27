e3dcae821ba7bb0ebd42fa3eb5cfa40144b8daa0
HofWe <tmszpilat@gmail.com>
2025-11-27 20:23:27 +0100

    first commit

---
+++ src/main/java/com/example/airlineticketingserviceapp/AirlineTicketingServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.airlineticketingserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineTicketingServiceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineTicketingServiceAppApplication.class, args);
+    }
+
+}
